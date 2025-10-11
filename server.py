ff052d9744ba883b6aedbce7c7b00da3c6de588d
Nicholas Trimble <Ntrimb10@Wgu.edu>
2025-10-11 15:32:18 -0500

    adjusted server file for deploy

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_mail import Mail, Message
+
+
+try:
+    from flask_mail import Mail, Message
+    MAIL_AVAILABLE = True
+except ImportError:
+    MAIL_AVAILABLE = False
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey123"
